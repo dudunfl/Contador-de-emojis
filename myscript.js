061d85163fa5f34d482a2d736703536d15fb7d75
Eduardo Galdini <dudunfl@hotmail.com>
2021-06-09 18:44:09 -0300

    add-to-commit

---
+++ myscript.js
@@ -0,0 +1,34 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0)
+        window.alert('[ERRO]Faltam Dados!')
+    else {
+        res.innerHTML = 'contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            window.alert('[PASSO INVÁLIDO] Considerando passo 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F916}`
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F47E}`
+            }
+        }
+
+        res.innerHTML += `\u{1F3C1}`
+    }
+
+}
